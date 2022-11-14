21c118e8f2fd91fb70f6604ed03879996d9bb0b4
Tammy <a0920760272@gmail.com>
2022-11-14 17:14:10 +0800

    add Step and ProgressBar component

--- src/components/StepProgress.js
+++ src/components/StepProgress.js
@@ -1,39 +1,33 @@
+
+// Step component
+function Step({text, label}) {
+  return (
+  <span className="progress-group">
+    <span className="progress-icon">
+      <span className="text">{text}</span>
+    </span>
+    <span className="progress-label">{label}</span>
+  </span>
+  );  
+}
+
+// ProgressBar component
+function ProgressBar() {
+  return (
+  <span className="progress-bar"></span>
+  );
+}
+
 export default function StepProgress() { 
    return(         
-        <div className="register-container col col-lg-6 col-sm-12" data-phase="1" data-total-price="0">
-          {/* register title */}
-          <h2 className="register-title col col-12">結帳</h2>
-          {/* register progress */}
-          <div className="progress-container col col-12">
-            <span className="progress-group" data-phase="address">
-              <span className="progress-icon">
-                <span className="text">1</span>
-                {/* <svg className="icon cursor-point">
-                  <use xlink:href="#svg-icon-pg-complete"></use>
-                </svg> */}
-              </span>
-              <span className="progress-label">寄送地址</span>
-            </span>
-            <span className="progress-bar" data-order="1"></span>
-            <span className="progress-group" data-phase="shipping">
-              <span className="progress-icon">
-                <span className="text">2</span>
-                {/* <svg className="icon cursor-point">
-                  <use xlink:href="#svg-icon-pg-complete"></use>
-                </svg> */}
-              </span>
-              <span className="progress-label">運送方式</span>
-            </span>
-            <span className="progress-bar" data-order="2"></span>
-            <span className="progress-group" data-phase="credit-card">
-              <span className="progress-icon">
-                <span className="text">3</span>
-                {/* <svg className="icon cursor-point">
-                  <use xlink:href="#svg-icon-pg-complete"></use>
-                </svg> */}
-              </span>
-              <span className="progress-label">付款資訊</span>
-            </span>
+        <div className="register-container">
+          <h2>結帳</h2>
+          <div className="progress-container">
+            <Step text="1" label="寄送地址"/>
+            <ProgressBar/>
+            <Step text="2" label="運送方式"/>
+            <ProgressBar/>
+            <Step text="3" label="付款資訊"/>
           </div>
         </div>
          );
