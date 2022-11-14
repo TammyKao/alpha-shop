7f477e8ae3e871560a4e488f17c8ab9d5904b9b9
Tammy <a0920760272@gmail.com>
2022-11-14 21:16:37 +0800

    add ProgressControl JSX

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import StepProgress from './components/StepProgress.js'
 import Step1 from './components/Step1.js'
 import Step2 from './components/Step2.js'
 import Step3 from './components/Step3.js'
+import ProgressControl from './components/ProgressControl.js'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
   <Step1/>
   <Step2/>
   <Step3/>
+  <ProgressControl/>
   </>
   );
 }
