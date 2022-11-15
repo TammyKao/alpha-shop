b7d8ab5ec1f90ea203f16ef425755644d9d65133
Tammy <a0920760272@gmail.com>
2022-11-15 18:14:40 +0800

    revise Step1 JSX

--- src/components/Step1.js
+++ src/components/Step1.js
@@ -1,81 +1,95 @@
 //  address phase
 
+const selectOptions = {
+title: [
+    {value:"mr", name:"先生"},
+    {value:"ms", name:"女士"},
+    {value:"mx", name:"不明"}
+],
+
+city: [                  
+    { value: "", name: "請選擇縣市" },
+    { value: "KLU", name: "基隆市" },
+    { value: "TPH", name: "新北市" },
+    { value: "TPE", name: "臺北市" },
+    { value: "TYC", name: "桃園市" },
+    { value: "HSH", name: "新竹縣" },
+    { value: "HSC", name: "新竹市" },
+    { value: "MAC", name: "苗栗市" },
+    { value: "MAL", name: "苗栗縣" },
+    { value: "TXG", name: "臺中市" },
+    { value: "CWH", name: "彰化縣" },
+    { value: "CWS", name: "彰化市" },
+    { value: "NTC", name: "南投市" },  
+    { value: "NTO", name: "南投縣" },
+    { value: "YLH", name: "雲林縣" },
+    { value: "CHY", name: "嘉義縣" },
+    { value: "CHI", name: "嘉義市" },
+    { value: "TNN", name: "臺南市" },
+    { value: "KHH", name: "高雄市" },
+    { value: "IUH", name: "屏東縣" },
+    { value: "PTS", name: "屏東市" },
+    { value: "ILN", name: "宜蘭縣" },
+    { value: "ILC", name: "宜蘭市" },  
+    { value: "HWA", name: "花蓮縣" },
+    { value: "HWC", name: "花蓮市" },
+    { value: "TTC", name: "臺東市" },
+    { value: "TTT", name: "臺東縣" },
+    { value: "PEH", name: "澎湖縣" },
+    { value: "KMN", name: "金門縣" },
+    { value: "LNN", name: "連江縣" }
+  ]
+}
+
+
+
+function SelectGroup ({label, options}) {
+  return (
+    <div className="input-group input-w-lg-2 input-w-sm-s1">
+      <div className="input-label">{label}</div>
+      <div className="select-container">
+        <select required>
+          { options.map( option => {
+            return <option value={option.value}>{option.name}</option>;
+            })}
+        </select>
+      </div>
+    </div>
+  );
+}
+
+
+
+function InputGroup ({label, type, placeholder}) {
+  return (
+    <div className="input-group">
+      <div className="input-label">{label}</div>
+      <input type={type} placeholder={placeholder} />
+    </div>
+  );
+} 
+
+
+
 export default function Step1 () {
-  return ( 
-            <form className="col col-12" data-phase="address">
-              <h3 className="form-title">寄送地址</h3>
-              <section className="form-body col col-12">
-                <div className="col col-12">
-                  <div className="input-group input-w-lg-2 input-w-sm-s1">
-                    <div className="input-label">稱謂</div>
-                    <div className="select-container">
-                      <select>
-                        <option value="mr" selected>先生</option>
-                        <option value="ms">女士</option>
-                        <option value="mx">不明</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="input-group input-w-lg-4 input-w-sm-s2">
-                    <div className="input-label">姓名</div>
-                    <input type="text" placeholder="請輸入姓名" />
-                  </div>
-                </div>
-                <div className="col col-12">
-                  <div className="input-group input-w-lg-3 input-w-sm-full">
-                    <div className="input-label">電話</div>
-                    <input type="tel" placeholder="請輸入行動電話" />
-                  </div>
-                  <div className="input-group input-w-lg-3 input-w-sm-full">
-                    <div className="input-label">Email</div>
-                    <input type="email" placeholder="請輸入電子郵件" />
-                  </div>
-                </div>
-                <div className="col col-12">
-                  <div className="input-group input-w-lg-2 input-w-sm-full">
-                    <div className="input-label">縣市</div>
-                    <div className="select-container">
-                      <select required>
-                        <option value="">請選擇縣市</option>
-                        <option value="KLU">基隆市</option>
-                        <option value="TPH">新北市</option>
-                        <option value="TPE">臺北市</option>
-                        <option value="TYC">桃園市</option>
-                        <option value="HSH">新竹縣</option>
-                        <option value="HSC">新竹市</option>
-                        <option value="MAC">苗栗市</option>
-                        <option value="MAL">苗栗縣</option>
-                        <option value="TXG">臺中市</option>
-                        <option value="CWH">彰化縣</option>
-                        <option value="CWS">彰化市</option>
-                        <option value="NTC">南投市</option>
-                        <option value="NTO">南投縣</option>
-                        <option value="YLH">雲林縣</option>
-                        <option value="CHY">嘉義縣</option>
-                        <option value="CYI">嘉義市</option>
-                        <option value="TNN">臺南市</option>
-                        <option value="KHH">高雄市</option>
-                        <option value="IUH">屏東縣</option>
-                        <option value="PTS">屏東市</option>
-                        <option value="ILN">宜蘭縣</option>
-                        <option value="ILC">宜蘭市</option>
-                        <option value="HWA">花蓮縣</option>
-                        <option value="HWC">花蓮市</option>
-                        <option value="TTC">臺東市</option>
-                        <option value="TTT">臺東縣</option>
-                        <option value="PEH">澎湖縣</option>
-                        <option value="KMN">金門縣</option>
-                        <option value="LNN">連江縣</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="input-group input-w-lg-4 input-w-sm-full">
-                    <div className="input-label">地址</div>
-                    <input type="text" placeholder="請輸入地址" />
-                  </div>
-                </div>
-              </section>
-            </form>
+  return (
+  <form className="col col-12" data-phase="address">
+    <h3 className="form-title">寄送地址</h3>
+    <section className="form-body col col-12">
+      <div className="col col-12">
+        <SelectGroup label='稱謂' options={selectOptions.title} />
+        <InputGroup label='姓名' type='text' placeholder='請輸入姓名' />
+      </div>
+      <div className="col col-12">
+        <InputGroup label='tel' type='text' placeholder='請輸入行動電話' />
+        <InputGroup label='email' type='text' placeholder='請輸入電子郵件' />
+      </div>
+      <div className="col col-12">
+        <SelectGroup label='城市' options={selectOptions.city}/>
+        <InputGroup label='地址' type='text' placeholder='請輸入地址' />
+      </div>
+    </section>
+  </form>
   );
 }
 
