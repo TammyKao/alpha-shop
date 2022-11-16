63c95a3265acd5efea4f3df309a4b04dced7623d
Tammy <a0920760272@gmail.com>
2022-11-16 11:50:23 +0800

    revise Step2 JSX

--- src/components/Step2.js
+++ src/components/Step2.js
@@ -1,11 +1,33 @@
 //  <!-- shipping phase -->
+
+function RadioGroup ({price, id, type, name, text, period}) {
+  return (
+  <label className="radio-group col col-12" data-price={price}>
+    <input id={id} type={type} name={name}  checked/>
+    <div className="radio-info">
+      <div className="col col-12">
+        <div className="text">{text}</div>
+        <div className="price"></div>
+      </div>
+      <div className="period col col-12">{period}</div>
+    </div>
+    <div className="radio-box-border"></div>
+  </label>
+  );
+}
+
+
+
+
 export default function Step2() {
   return (
     <form class="col col-12" data-phase="shipping">
     <div>
       <h3 className="form-title">運送方式</h3>
       <section className="form-body col col-12">
-        <label className="radio-group col col-12" data-price="0">
+        <RadioGroup id="shipping-standard" type="radio" name="shipping" price="0" text="標準運送"  period="約 3~7 個工作天" />
+        <RadioGroup id="shipping-dhl" type="radio" name="shipping" price="500" text="DHL 貨運"  period="48 小時內送達" />
+        {/* <label className="radio-group col col-12" data-price="0">
           <input id="shipping-standard" type="radio" name="shipping"  checked/>
             <div className="radio-info">
                     <div className="col col-12">
@@ -15,8 +37,8 @@ export default function Step2() {
                     <div className="period col col-12">約 3~7 個工作天</div>
             </div>
             <div className="radio-box-border"></div>
-        </label>
-        <label className="radio-group col col-12" data-price="500">
+        </label> */}
+        {/* <label className="radio-group col col-12" data-price="500">
             <input id="shipping-dhl" type="radio" name="shipping" />
               <div className="radio-info">
                 <div className="col col-12">
@@ -26,7 +48,7 @@ export default function Step2() {
                 <div className="period col col-12">48 小時內送達</div>
                 </div>
                 <div className="radio-box-border"></div>
-        </label>
+        </label> */}
       </section>
     </div>
     </form>
