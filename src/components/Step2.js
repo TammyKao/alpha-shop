cbe51f0e7190603348eac34dfaf00345943d1a22
Tammy <a0920760272@gmail.com>
2022-11-17 17:42:48 +0800

    revise Step2 JSX and set Step2 css

--- src/components/Step2.js
+++ src/components/Step2.js
@@ -1,17 +1,19 @@
 //  <!-- shipping phase -->
 
-function RadioGroup ({price, id, type, name, text, period}) {
+import styles from './Step2.module.css'
+
+function RadioGroup ({price, id, text, priceText, period, checked}) {
   return (
-  <label className="radio-group col col-12" data-price={price}>
-    <input id={id} type={type} name={name}  checked/>
-    <div className="radio-info">
+  <label className={styles.radio__group} data-price={price}>
+    <input id={id} type="radio" name="shipping"  defaultChecked={checked}/>
+    <div className={styles.radio__info}>
       <div className="col col-12">
-        <div className="text">{text}</div>
-        <div className="price"></div>
+        <div className={styles.text}>{text}</div>
+        <div className={styles.price}>{priceText}</div>
       </div>
-      <div className="period col col-12">{period}</div>
+      <div className={styles.period}>{period}</div>
     </div>
-    <div className="radio-box-border"></div>
+    <div className={styles.radio__box__border}></div>
   </label>
   );
 }
@@ -19,25 +21,25 @@ function RadioGroup ({price, id, type, name, text, period}) {
 
 export default function Step2() {
   return (
-    <form class="col col-12" data-phase="shipping">
+    <form class={styles.form__container} data-phase="shipping">
     <div>
-      <h3 className="form-title">運送方式</h3>
-      <section className="form-body col col-12">
+      <h3 className={styles.form__title}>運送方式</h3>
+      <section className={styles.form__body}>
         <RadioGroup 
           id="shipping-standard" 
-          type="radio" 
-          name="shipping" 
           price="0" 
-          text="標準運送"  
+          text="標準運送" 
+          priceText="免費" 
           period="約 3~7 個工作天"
+          checked="true"
          />
         <RadioGroup 
-          id="shipping-dhl" 
-          type="radio" 
-          name="shipping" 
+          id="shipping-dhl"  
           price="500" 
-          text="DHL 貨運"  
+          text="DHL 貨運"
+          priceText="$500"  
           period="48 小時內送達"
+          checked="false"
          />
       </section>
     </div>
