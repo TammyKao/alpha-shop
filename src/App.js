0010ac73826a2516a40de79e678d600051ca0b50
Tammy <a0920760272@gmail.com>
2022-11-19 01:18:52 +0800

    revise ProgressStep JSX and css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './base.module.css';
 import ProgressStep from './components/ProgressStep.js'
+// 先渲染 Step1畫面，以下Step2 & Step3 將於後續實作換頁時呈現
 import Step1 from './components/Step1.js'
-import Step2 from './components/Step2.js'
-import Step3 from './components/Step3.js'
+// import Step2 from './components/Step2.js'
+// import Step3 from './components/Step3.js'
 import ProgressControl from './components/ProgressControl.js'
 
 function App() {
@@ -10,8 +11,6 @@ function App() {
   <>
   <ProgressStep/>
   <Step1/>
-  <Step2/>
-  <Step3/>
   <ProgressControl/>
   </>
   );
--- src/components/ProgressStep.js
+++ src/components/ProgressStep.js
@@ -1,10 +1,10 @@
 import styles from './ProgressStep.module.css'
 
 // Step component
-function Step({text, label}) {
+function Step({phase, text, label}) {
   return (
   <span className={styles.progress__group}>
-    <span className={styles.progress__icon}>
+    <span className={styles.progress__icon} phase={phase}>
       <span className={styles.progress__text}>{text}</span>
     </span>
     <span className={styles.progress__label}>{label}</span>
@@ -13,22 +13,34 @@ function Step({text, label}) {
 }
 
 // ProgressBar component
-function ProgressBar() {
+function ProgressBar({className}) {
   return (
-  <span className={styles.progress__bar}></span>
+  <span className={className}></span>
   );
 }
 
 export default function ProgressStep() { 
    return (
    <div>
-     <h2 className={styles.register__title}>結帳</h2>
+     <h2 className={styles.progress__title}>結帳</h2>
      <section className={styles.progress__container}>
-       <Step text="1" label="寄送地址"/>
-       <ProgressBar/>
-       <Step text="2" label="運送方式"/>
-       <ProgressBar/>
-       <Step text="3" label="付款資訊"/>
+       <Step 
+         text="1" 
+         label="寄送地址" 
+         phase="done"
+       />
+       <ProgressBar className={styles.progress__bar} />
+       <Step 
+         text="2" 
+         label="運送方式" 
+         phase="undone"
+         />
+       <ProgressBar className={styles.progress__bar__undone} />
+       <Step 
+         text="3" 
+         label="付款資訊" 
+         phase="undone"
+         />
      </section>
    </div>
    );
