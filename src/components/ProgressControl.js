dab972b7048cf998d5e518fc34cc1d7c29163b83
Tammy <a0920760272@gmail.com>
2022-11-18 16:11:20 +0800

    add and import svg icons to ProgressControl.jsx

--- src/components/ProgressControl.js
+++ src/components/ProgressControl.js
@@ -1,3 +1,5 @@
+import styles from './ProgressControl.module.css'
+
 function Button({name, text}) {
   return (
     <button class={name}>
@@ -9,15 +11,15 @@ function Button({name, text}) {
 
 export default function ProgressControl() {
   return (
-  <section class="progress-control-container col col-lg-6 col-sm-12">
-    <section class="button-group col col-12" data-phase="address">
+  <section class={styles.progress__control__container}>
+    <section class={styles.button__container} data-phase="address">
       <Button name="next" text="下一步"/>
     </section>
-    <section class="button-group col col-12" data-phase="shipping">
+    <section class={styles.button__container} data-phase="shipping">
       <Button name="prev" text="上一步"/>
       <Button name="next" text="下一步"/>
     </section>
-    <section class="button-group col col-12" data-phase="credit-card">
+    <section class={styles.button__container} data-phase="credit-card">
       <Button name="prev" text="上一步"/>
       <Button name="next" text="確認下單"/>
     </section>
