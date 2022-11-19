e4ffc552f6f0b0e3d88228cf0b33d5028b69c091
Tammy <a0920760272@gmail.com>
2022-11-19 15:48:48 +0800

    revise App.js and imort App.module.css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import './base.module.css';
+import styles from './App.module.css';
 import ProgressStep from './components/ProgressStep.js'
-// 先渲染 Step1畫面，以下Step2 & Step3 將於後續實作換頁時呈現
 import Step1 from './components/Step1.js'
+
+// 此階段先渲染 Step1畫面，以下Step2 & Step3 將於後續實作換頁時呈現
 // import Step2 from './components/Step2.js'
 // import Step3 from './components/Step3.js'
+
 import ProgressControl from './components/ProgressControl.js'
 
 function App() {
   return (
-  <>
+  <div className={styles.main__container}>
   <ProgressStep/>
   <Step1/>
   <ProgressControl/>
-  </>
+  </div>
   );
 }
 
